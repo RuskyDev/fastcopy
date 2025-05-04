c7bbae9076870e1e49bf4dbf9400ad80bfc7a585
Rusky <91284372+RuskyDev@users.noreply.github.com>
2025-05-04 15:27:58 +0500

    Update script for cross-platform build support (Windows, Linux, macOS)

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-[ -f "bin/FastCopy.exe" ] && rm "bin/FastCopy.exe"
-go build -o "bin/FastCopy.exe" src/main.go src/args.go src/chunk.go
+platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+
+rm -rf bin/*
+
+for platform in "${platforms[@]}"; do
+  GOOS=$(echo $platform | cut -d'/' -f1)
+  GOARCH=$(echo $platform | cut -d'/' -f2)
+
+  if [ "$GOOS" == "windows" ]; then
+    output="bin/windows/FastCopy.exe"
+  elif [ "$GOOS" == "linux" ]; then
+    output="bin/linux/FastCopy"
+  elif [ "$GOOS" == "darwin" ]; then
+    output="bin/macos/FastCopy"
+  fi
+
+  mkdir -p "$(dirname "$output")"
+
+  GOOS=$GOOS GOARCH=$GOARCH go build -o "$output" src/main.go src/args.go src/chunk.go
+done
